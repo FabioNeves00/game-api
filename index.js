b3142bd60f6433134def545a0e346418f0c36601
Newton Fernando de Souza Miranda Filho <newton.chickengopro@gmail.com>
2021-10-03 16:00:58 -0300

    Adding Cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import routes from './src/routes/index.js';
 
 dotenv.config();
 const app = express();
 const PORT = Number(process.env.PORT) || 5500;
 
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', routes);
