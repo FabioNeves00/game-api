e2802916024d5e3a07df215d40dc322f7c63dbe1
Newton Fernando de Souza Miranda Filho <newton.chickengopro@gmail.com>
2021-10-03 16:23:40 -0300

    Route Test

--- index.js
+++ index.js
@@ -12,4 +12,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use('/api', routes);
 
+app.get('/', (req, res) => res.send({ Hello: 'World!' }));
+
 app.listen(PORT, () => console.log(`API REST running in PORT: ${PORT}`));
