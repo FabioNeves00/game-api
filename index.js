c666daa20fef9dda67c9eac17a6aa7625aec5015
Newton Fernando de Souza Miranda Filho <newton.chickengopro@gmail.com>
2021-10-03 15:12:16 -0300

    Init API

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log('Hello World');
+import express from 'express';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = Number(process.env.PORT) || 5500;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+app.listen(PORT, () => console.log(`API REST running in PORT: ${PORT}`));
