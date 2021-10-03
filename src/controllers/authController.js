14abd1cd1c4c6754b0ca833967c515d86169b0c5
Newton Fernando de Souza Miranda Filho <newton.chickengopro@gmail.com>
2021-10-03 15:44:48 -0300

    authController - Register

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,10 +1,53 @@
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 import dotenv from 'dotenv';
+import { validationResult } from 'express-validator';
+import User from '../models/User.js';
 
 dotenv.config();
 
+function generateToken(params = {}) {
+  return jwt.sign(params, process.env.SECRET_KEY, {
+    expiresIn: 86400,
+  });
+}
+
 class AuthController {
   async register(req, res) {
-    return res.send({ route: 'register' });
+    const { name, email, password } = req.body;
+    const validator = validationResult(req);
+
+    try {
+      if (validator.errors.length) {
+        const errors = [];
+
+        // eslint-disable-next-line no-restricted-syntax
+        for (const error of validator.errors) {
+          errors.push(`This ${error.param} is invalid.`);
+        }
+
+        // Este filtro eu coloquei pra corrigir um erro de return
+        // Caso saiba como solucionar,pf deixe uma msg pra mim t-t. Obg
+        return res.status(400).send({
+          error: errors.filter((item, pos) => errors.indexOf(item) === pos),
+        });
+      }
+
+      if (await User.findOne({ email }))
+        return res.status(400).send({ error: 'This email already exists.' });
+
+      const user = await User.create({
+        name,
+        email,
+        password: await bcrypt.hash(password, 10),
+      });
+
+      user.password = undefined;
+
+      return res.send({ user, token: generateToken({ id: user.id }) });
+    } catch (err) {
+      return res.status(400).send({ error: 'Registration failed.' });
+    }
   }
 
   async authenticate(req, res) {
